84c2d754bef2228fab646656f4a87cc70e1521bf
Portian02 <135663498+Portian02@users.noreply.github.com>
2023-09-11 11:42:24 -0600

    update calculator

--- calculator/spec/calculator_spec.rb
+++ calculator/spec/calculator_spec.rb
@@ -2,20 +2,17 @@ require "spec_helper"
 require_relative '../calculator.rb'
 
 describe "calculator" do 
+    let(:calculator) {calculator.new}
     it "suma dos número positivos" do
-        calculator = Calculator.new
         expect(calculator.add(2,3)).to eq(5)
     end
     it "resta dos números positivos"do
-        calculator = Calculator.new
         expect(calculator.subtract(5,2)).to eq(3)
     end
     it "multiplicar dos números positivos"do
-    calculator = Calculator.new
     expect(calculator.multiply(5,2)).to eq(10)
     end
     it "dividir dos números positivos"do
-        calculator = Calculator.new
         expect{calculator.divide(5,0)}.to raise_error(StandardError, "No se puede dividir por cero")
     end
 end
